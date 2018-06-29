b64aba4bc12d11dab7c9dd2c975aa0d4444f1239
Vikram <gopuvikram818@gmail.com>
2018-06-29 11:00:29 +0200

    added cose support

--- src/edhocClient.c
+++ src/edhocClient.c
@@ -7,12 +7,34 @@
 #include <stdio.h>
 #include <stdio.h>
 #include "cryptoFunctions.h"
+#include <cose-c/cose.h>
 
 int main(void) {
 	/* Generate private and public keys */
 	EVP_PKEY *pkey = genX25519KeyPair();
-	printf("\nBob's PUBKEY:\n");
+	printf("\nAlice's PUBKEY:\n");
 	PEM_write_PUBKEY(stdout, pkey);
+
+	/* Write public key to file */
+	BIO *out;
+	out = BIO_new_file("../inputparameters/server_PUBKEY.txt", "w+");
+
+	if (!out) {
+		/* Error */
+		printf("BIO out is empty\n");
+	}
+	PEM_write_bio_PUBKEY(out, pkey);
+	BIO_flush(out);
+
+	/* Read Alice's public key */
+	FILE *keyfile = fopen("../inputparameters/server_PUBKEY.txt", "r");
+	EVP_PKEY *peerkey = NULL;
+	peerkey = PEM_read_PUBKEY(keyfile, NULL, NULL, NULL);
+	printf("\nAlice's PUBKEY:\n");
+	PEM_write_PUBKEY(stdout, peerkey);
+
+	/*check cose working properly*/
+	printf("cose enum value: %d\n", COSE_sign_object);
 	return 0;
 }
 
---
+++ src/edhocMessage1.h
@@ -0,0 +1,11 @@
+#include "cbor.h"
+#include "cryptoFunctions.h"
+#include <openssl/evp.h>
+
+/** function to generate edhoc message1 */
+unsigned char *genMsg1Sym(unsigned char *app_1, size_t app_1_sz, EVP_PKEY *pkey,
+		const char *filepath);
+void *parseEdhocSymMsg1(cbor_item_t *MSG);
+
+size_t message_1_len;
+unsigned char *message_1;
